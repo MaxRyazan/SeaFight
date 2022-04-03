4704dea55578e3cc2eaf2a53ea4f4abc455d4002
Max <no4ta87@mail.ru>
2022-04-03 15:45:08 +0300

    version 3.1 inject spring annotations

---
+++ src/main/java/ru/maxruazan/seafight/SpringConfigurationClass.java
@@ -0,0 +1,10 @@
+package ru.maxruazan.seafight;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.maxruazan.seafight")
+public class SpringConfigurationClass {
+}
--- src/main/java/ru/maxruazan/seafight/SpringMain.java
+++ src/main/java/ru/maxruazan/seafight/SpringMain.java
@@ -1,10 +1,12 @@
 package ru.maxruazan.seafight;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 public class SpringMain {
     public static void main(String[] args) {
-      ClassPathXmlApplicationContext context =  new ClassPathXmlApplicationContext("config.xml");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SpringConfigurationClass.class);
 
       Game game = context.getBean("game", Game.class);
       game.play();
